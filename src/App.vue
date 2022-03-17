3eb6ac8bba14d201bb452a90f1d07ffd5373c9aa
igor-makariew <34421941+igor-makariew@users.noreply.github.com>
2022-03-17 21:53:37 +0300

    create template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,141 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="#">dirEngine.</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                    aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto" id="active_menu">
+                    <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Tour</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Hotels</a></li>
+                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Contact</a></li>
+                    <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>Add listing</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main role="main" class="flex-shrink-0">
+        <div>
+
+            <router-view/>
+
+        </div>
+    </main>
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">dirEngine</h2>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                        <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4 ml-md-5">
+                        <h2 class="ftco-heading-2">Information</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">About</a></li>
+                            <li><a href="#" class="py-2 d-block">Service</a></li>
+                            <li><a href="#" class="py-2 d-block">Terms and Conditions</a></li>
+                            <li><a href="#" class="py-2 d-block">Become a partner</a></li>
+                            <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li>
+                            <li><a href="#" class="py-2 d-block">Privacy and Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Customer Support</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">FAQ</a></li>
+                            <li><a href="#" class="py-2 d-block">Payment Option</a></li>
+                            <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
+                            <li><a href="#" class="py-2 d-block">How it works</a></li>
+                            <li><a href="#" class="py-2 d-block">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="ftco-footer-widget mb-4">
+                        <h2 class="ftco-heading-2">Have a Questions?</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span>
+                                </li>
+                                <li><a href="#"><span class="icon icon-phone"></span><span
+                                        class="text">+2 392 3929 210</span></a></li>
+                                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+
+                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;
+                        All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i>
+                        by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
+<script>
+    export default {
+        data() {
+            return {}
+        },
+
+        mounted() {
+            this.$nextTick( () => {
+                $('#app').hide();
+                setTimeout( () => {
+                    $('#app').fadeIn('slow');
+                }, 1000)
+            })
+        }
+    }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-nav {
-  padding: 30px;
-}
+    nav {
+        padding: 30px;
+    }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    nav a.router-link-exact-active {
+        color: #42b983;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 import { createApp } from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import jQuery from 'jquery/dist/jquery.min';
+global.jQuery = jQuery;
+global.$ = jQuery;
+let Bootstrap = require('bootstrap/dist/js/bootstrap.min');
+import Popper from 'popper.js/dist/popper';
+import jQueryEasing from 'jquery.easing/jquery.easing.min';
+import jQueryWaypoints from 'jquery-waypoints/waypoints.min';
+let jQueryStellar  = require('jquery.stellar/jquery.stellar');
+let Carousel  = require('owl.carousel/dist/owl.carousel');
+import MagnificPopup from 'magnific-popup/dist/jquery.magnific-popup';
+import AOS from 'aos/dist/aos';
+// import AOS from 'aos'
+let AnimateNumber = require('jquery.animate-number/jquery.animateNumber');
+import  Scrollax from 'scrollax/scrollax.min';
+// let MainJs =  require('./assets/js/main');
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '@/assets/css/animate.css';
+import '@/assets/css/aos.css';
+import '@/assets/css/bootstrap-datepicker.css';
+import '@/assets/css/flaticon.css';
+import '@/assets/css/icomoon.css';
+import '@/assets/css/ionicons.min.css';
+import '@/assets/css/jquery.timepicker.css';
+import '@/assets/css/magnific-popup.css';
+import '@/assets/css/open-iconic-bootstrap.min.css';
+import '@/assets/css/owl.theme.default.min.css';
+import '@/assets/css/style.css';
 
 createApp(App)
     .use(store)
