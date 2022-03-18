e638ad20dcb265b678ac1bce68a1dd9e03090394
igor-makariew <34421941+igor-makariew@users.noreply.github.com>
2022-03-18 16:41:18 +0300

    create new template

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  // resolve: {
-  //   alias: {
-  //     core: path.join(__dirname, 'core'),
-  //   },
-  // },
+  transpileDependencies: true
 })
